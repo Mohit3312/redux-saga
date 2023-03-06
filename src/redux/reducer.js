18701568548225ecbd48bfc1dfcb36aa35c88b37
Mohit3312 <mohit3312@gmail.com>
2023-03-06 17:38:27 +0530

    Used switch case instead of if else in reducer.js

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,10 +1,20 @@
 import { ADD_TO_CART } from "./constant";
 
 export const cartData = (data = [], action) => {
-  if (action.type === ADD_TO_CART) {
-    console.log("Reducer called", action);
-    return action.data;
-  } else {
-    return "no action matched";
+  //   if (action.type === ADD_TO_CART) {
+  //     console.log("Reducer called", action);
+  //     return action.data;
+  //   } else {
+  //     return "no action matched";
+  //   }
+
+  switch (action.type) {
+    case ADD_TO_CART:
+      // Logic for add to cart
+      console.log("ADD_TO_CART", action);
+      return 1 + 1;
+
+    default:
+      return "no action matched";
   }
 };
