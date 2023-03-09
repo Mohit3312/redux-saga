c2b492d7787d82e17fb3070a85c119428cfc0aa9
Mohit3312 <mohit3312@gmail.com>
2023-03-09 23:45:36 +0530

    Product list UI updated

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,57 +1,91 @@
 import { useDispatch, useSelector } from "react-redux";
 import { addToCart, removeFromCart, emptyCart } from "../redux/action";
 import { productList } from "../redux/productAction";
+import { useEffect } from "react";
 
 function Main() {
   const dispatch = useDispatch();
   const productDataResult = useSelector((state) => state.productData);
   console.log("Data in main component from saga", productDataResult);
-  const product = {
-    name: "GT Master",
-    color: "white",
-    type: "mobile",
-    price: 26000,
-  };
+
+  useEffect(() => {
+    dispatch(productList());
+    // eslint-disable-next-line
+  }, []);
+
   return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "space-evenly",
-        margin: "1rem",
-      }}
-    >
-      <button
-        onClick={() => {
-          dispatch(addToCart(product));
+    <>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-evenly",
+          margin: "1rem",
         }}
       >
-        Add to Cart
-      </button>
-
-      <button
-        onClick={() => {
-          dispatch(removeFromCart(product.name));
+        <button
+          onClick={() => {
+            dispatch(emptyCart());
+          }}
+        >
+          Empty Cart
+        </button>
+      </div>
+      <div
+        className="product-container"
+        style={{
+          marginTop: "50px",
+          display: "flex",
+          flexWrap: "wrap",
+          justifyContent: "space-evenly",
+          alignItems: "center",
         }}
       >
-        Remove from Cart
-      </button>
-
-      <button
-        onClick={() => {
-          dispatch(emptyCart());
-        }}
-      >
-        Empty Cart
-      </button>
-
-      <button
-        onClick={() => {
-          dispatch(productList());
-        }}
-      >
-        Get Product List
-      </button>
-    </div>
+        {productDataResult.map((item) => (
+          <div
+            key={item.id}
+            className="product-item"
+            style={{
+              display: "flex",
+              alignItems: "center",
+              flexDirection: "column",
+              border: "2px solid black",
+              padding: "10px",
+              borderRadius: "15px",
+            }}
+          >
+            <img src={item.photo} height="100px" alt="" />
+            <div style={{ marginTop: "25px" }}>Name: {item.name}</div>
+            <div>Color: {item.color}</div>
+            <div>Price: {item.price}</div>
+            <div>Category: {item.category}</div>
+            <div>Brand: {item.brand}</div>
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "space-between",
+                width: "250px",
+                marginTop: "25px",
+              }}
+            >
+              <button
+                onClick={() => {
+                  dispatch(addToCart(item));
+                }}
+              >
+                Add to Cart
+              </button>
+              <button
+                onClick={() => {
+                  dispatch(removeFromCart(item.id));
+                }}
+              >
+                Remove from Cart
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
   );
 }
 
